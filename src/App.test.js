e6453bc324a9e89b602f7b8279e3c3129aab17f4
Isis van der Plas <isisvanderplas@hotmail.com>
2017-11-28 12:00:39 +0100

    create app & title with testsuites

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { shallow } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import App from './App'
+import Title from './components/Title'
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+chai.use(chaiEnzyme())
+
+
+
+describe('<App />', () => {
+  const app = shallow(<App />)
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('wraps everything in a div tag', () => {
+    expect(app).to.have.tagName('div')
+  })
+
+  it('contains a Title', () => {
+    expect(app).to.have.descendants(Title)
+  })
+
+  it('sets the Title to "All Recipes"', () => {
+    expect(app).to.contain(<Title content="All Recipes" />)
+  })
+})
